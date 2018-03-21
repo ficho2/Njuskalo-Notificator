96c2f9dcc5ab01a3d3bac94e7b5b4287da8dfa1a
Filip Kuna <ficho2@users.noreply.github.com>
2018-03-21 21:02:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def kreni(url,price,oib,maddr,fdata):
 
     oglasi=[]
     for item in url:
-        #print i,price[i]
         try:
             oglasi=sc.getOglasi(item,price[i])
         except:
@@ -25,9 +24,9 @@ def kreni(url,price,oib,maddr,fdata):
     if flag==1:
         flag=0
         print msg
-        #print "radim"
         #**********************************************************************
-        #Maknuti # ispred mail/face.sendMail da bi se slala obavjest tim putem
+        #Maknuti # ispred mail/face.sendMail da bi se slala obavjest tim putem ili staviti da ne bi
+        #Remove # befor mail.sendMail/ face.sendMail to start notificator
         #**********************************************************************
         try:
             mail.sendMail(msg,maddr)
@@ -44,19 +43,22 @@ def kreni(url,price,oib,maddr,fdata):
 oib=[]
 #***************************************************************************************************************************************************************
 # U url ubacujes linkove koje da pregleda
+# url - list of urls to keep track of
 url=['http://www.njuskalo.hr/iphone-7-plus', 'http://www.njuskalo.hr/iphone-7', 'http://www.njuskalo.hr/iphone-se', 'http://www.njuskalo.hr/iphone-6s-plus',
      'http://www.njuskalo.hr/iphone-6-plus', 'http://www.njuskalo.hr/iphone-6s', 'http://www.njuskalo.hr/iphone-6', 'http://www.njuskalo.hr/iphone-5s',
      'http://www.njuskalo.hr/iphone-5c', 'http://www.njuskalo.hr/iphone-5']
 
 #U price ubacujes cijene za svaku kategoriju, moraju biti u istom redosljedu ko i linkovi
+#price - upper target price for notification 
 price=[3500,3500,1800,1800,1800,1600,1600,700,600,500]
 
-
-maddr=["user_name@gmail.com","primatelj@gmail.com","sifra"]
 #['mail sa kojeg saljes','mail koji prima','sifra od maila sa kojeg saljes']
+#information for mail
+maddr=["sender@gmail.com","reciver@gmail.com","password"]
 
-fdata=["user_name@nesto.nes", "sifra", "ime prijatelja"]
 #["mail od fejsa sa kojeg se salje","sifra od fejsa","ime na koje zelite slat"]
+fdata=["sender@nesto.nes", "password", "reciver"]
+
 
 #***************************************************************************************************************************************************************
 
